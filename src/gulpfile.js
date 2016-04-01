4c170513ce4a2a1dd30e0ad2e11ef8d1585d0010
Kinice <szp93@126.com>
2016-04-01 17:11:19 +0800

    oversea continues

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -6,12 +6,26 @@ var gulp = require('gulp'),
     concat = require('gulp-concat'),
     notify = require('gulp-notify');
 
+var Browsersync = require('browser-sync').create();
+var reload      = Browsersync.reload;
+
 gulp.task('styles', function() {
   return gulp.src('./static/less/*.less')
     .pipe(less())
     .pipe(minifycss())
     .pipe(gulp.dest('../public/styles'))
+    .pipe(reload({stream: true}))
     .pipe(notify({ message: 'styles task complete' }));
 });
-
-gulp.task('default', ['styles']);
+gulp.task('browser-sync',['styles'], function() {
+    Browsersync.init({
+        proxy: "http://localhost:63342/oversea/views/index.html",
+        baseDir:"./",
+        port: 63342,
+        uiPort: 63343
+    });
+    gulp.watch('./static/less/*.less', ['styles']);
+    gulp.watch('../views/*.html').on('change', reload);
+    gulp.watch('./static/script/*.js').on('change', reload);
+});
+gulp.task('default', ['browser-sync']);
