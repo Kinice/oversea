aeeb7d7d93e3e883c419935e4140d260fc609496
kinice <szp93@126.com>
2016-04-04 21:32:41 +0800

    .

--- public/scripts/jquery.slider.js
+++ public/scripts/jquery.slider.js
@@ -1,5 +1,5 @@
 ;(function(){
-  var $con = $('.m-slider');
+  /*var $con = $('.m-slider');
   var timer = function(t,max){
     if(t<=max){
       t += 1
@@ -13,5 +13,28 @@
     var items = $container.find('.slider-item');
     setInterval(timer(index,3),1000);
   }
-  slider($con);
+  slider($con);*/
+  var slider = {
+    $con : $('.slider-item'),
+    index : 0,
+    slide : function(index){
+      this.$con.eq(index).siblings().animate({
+        opacity : 0
+      },1600);
+      this.$con.eq(index).animate({
+        opacity : 1
+      },1600);
+    },
+    autoShow : function(){
+      this.index += 1;
+      if(this.index <= 2 - 1){
+          slider.slide(this.index);
+      }else{
+          this.index = 0;
+          slider.slide(this.index);
+      }
+      setInterval(this.autoShow,5000);
+    }
+  }
+  slider.autoShow();
 })();
