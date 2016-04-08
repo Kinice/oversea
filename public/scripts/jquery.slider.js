db9875a5db7837343fcd5433ee2565dd7905739c
Kinice <szp93@126.com>
2016-04-08 16:36:25 +0800

    .

--- public/scripts/jquery.slider.js
+++ public/scripts/jquery.slider.js
@@ -1,29 +1,20 @@
 ;(function(){
-  /*var $con = $('.m-slider');
-  var timer = function(t,max){
-    if(t<=max){
-      t += 1
-    }else {
-      t = 0;
-    }
-    console.log(t);
-  }
-  var slider = function($container){
-    var index = 0;
-    var items = $container.find('.slider-item');
-    setInterval(timer(index,3),1000);
-  }
-  slider($con);*/
   var slider = {
     $con : $('.slider-item'),
     index : 0,
+    animateTime : 800,
+    slideTime : 5000,
     slide : function(index){
-      this.$con.eq(index).siblings().animate({
+      this.$con.eq(index).siblings('.slider-item').animate({
         opacity : 0
-      },1600);
+      },this.animateTime);
+      timer = setTimeout(function(){
+          this.$con.eq(index).siblings('.slider-item').css('display','none');
+      }.bind(this),this.animateTime);
+      this.$con.eq(index).css('display','block');
       this.$con.eq(index).animate({
         opacity : 1
-      },1600);
+      },800);
     },
     autoShow : function(){
       this.index += 1;
@@ -35,9 +26,54 @@
       }
     },
     init : function(){
+      var max = $('.slider-item').length;
+      var isAnimating = false;
       this.$con.eq(0).css('opacity','1');
-      this.$con.eq(0).siblings().css('opacity','0');
-      setInterval(this.autoShow.bind(this),5000);
+      this.$con.eq(0).siblings('.slider-item').css('opacity','0');
+      intimer = setInterval(this.autoShow.bind(this),this.slideTime);
+
+        $('.g-slider').hover(function(){
+            clearInterval(intimer);
+        },function(){
+            intimer = setInterval(this.autoShow.bind(this),this.slideTime);
+        }.bind(this));
+
+        $('.arrow-r').click(function(){
+            if(!isAnimating){
+                isAnimating = true;
+                this.index += 1;
+                if(this.index <= max-1){
+                    this.slide(this.index);
+                }else{
+                    this.index = 0;
+                    this.slide(this.index)
+                }
+                setTimeout(function(){
+                    isAnimating = false;
+                },this.animateTime);
+            }else{
+                return false;
+            }
+        }.bind(this));
+
+        $('.arrow-l').click(function(){
+            if(!isAnimating) {
+                isAnimating = true;
+                this.index -= 1;
+                if (this.index < 0) {
+                    this.index = max - 1;
+                    this.slide(this.index);
+                } else {
+                    this.slide(this.index)
+                }
+                setTimeout(function(){
+                    isAnimating = false;
+                },this.animateTime);
+                console.log(this.index);
+            }else{
+                return false;
+            }
+        }.bind(this));
     }
   }
   slider.init();
